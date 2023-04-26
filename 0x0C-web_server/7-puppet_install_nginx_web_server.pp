2cf2ff805ade21c436be53084ce534d3eb6b81e2
Davidnna <davedozie1@gmail.com>
2023-04-26 15:16:20 -0700

    Done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,24 @@
-# Install Nginx web server (w/ Puppet)
-package { 'nginx':
-  ensure => installed,
+  
+# Install NginX
+# With puppet
+
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
 }
 
-file_line { 'aaaaa':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get-update'],
 }
 
 file { '/var/www/html/index.html':
   content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+exec {'redirect_me':
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
 }
 
 service { 'nginx':
--- 0x0C-web_server/README.md
+++ 0x0C-web_server/README.md
@@ -1,1 +1,2 @@
-0x0C-web_server
+# 0x0C-web_server
+to be done.
